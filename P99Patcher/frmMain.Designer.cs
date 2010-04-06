9559a18a523c21a13d0e45d9b5212b9a5405189b
Michael Bolland <mixxit@soliniaonline.com>
2010-04-06 21:15:55 +0100

    Initial import

---
+++ P99Patcher/frmMain.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace P99Patcher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnWeb = new System.Windows.Forms.Button();
+            this.btnAccount = new System.Windows.Forms.Button();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.txtPatchNotes = new System.Windows.Forms.TextBox();
+            this.btnMinimise = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.ForeColor = System.Drawing.Color.White;
+            this.btnPlay.Location = new System.Drawing.Point(288, 355);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(99, 25);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnWeb
+            // 
+            this.btnWeb.BackColor = System.Drawing.Color.Transparent;
+            this.btnWeb.FlatAppearance.BorderSize = 0;
+            this.btnWeb.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnWeb.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnWeb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWeb.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWeb.ForeColor = System.Drawing.Color.White;
+            this.btnWeb.Location = new System.Drawing.Point(404, 362);
+            this.btnWeb.Name = "btnWeb";
+            this.btnWeb.Size = new System.Drawing.Size(90, 17);
+            this.btnWeb.TabIndex = 1;
+            this.btnWeb.Text = "Web";
+            this.btnWeb.UseVisualStyleBackColor = false;
+            this.btnWeb.Click += new System.EventHandler(this.btnWeb_Click);
+            // 
+            // btnAccount
+            // 
+            this.btnAccount.BackColor = System.Drawing.Color.Transparent;
+            this.btnAccount.FlatAppearance.BorderSize = 0;
+            this.btnAccount.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnAccount.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccount.ForeColor = System.Drawing.Color.White;
+            this.btnAccount.Location = new System.Drawing.Point(500, 362);
+            this.btnAccount.Name = "btnAccount";
+            this.btnAccount.Size = new System.Drawing.Size(84, 17);
+            this.btnAccount.TabIndex = 2;
+            this.btnAccount.Text = "Account";
+            this.btnAccount.UseVisualStyleBackColor = false;
+            this.btnAccount.Click += new System.EventHandler(this.btnAccount_Click);
+            // 
+            // btnOptions
+            // 
+            this.btnOptions.BackColor = System.Drawing.Color.Transparent;
+            this.btnOptions.FlatAppearance.BorderSize = 0;
+            this.btnOptions.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnOptions.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnOptions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOptions.ForeColor = System.Drawing.Color.White;
+            this.btnOptions.Location = new System.Drawing.Point(546, 26);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Size = new System.Drawing.Size(45, 18);
+            this.btnOptions.TabIndex = 3;
+            this.btnOptions.Text = "Options";
+            this.btnOptions.UseVisualStyleBackColor = false;
+            this.btnOptions.Click += new System.EventHandler(this.btnOptions_Click);
+            // 
+            // txtPatchNotes
+            // 
+            this.txtPatchNotes.BackColor = System.Drawing.Color.Black;
+            this.txtPatchNotes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPatchNotes.Enabled = false;
+            this.txtPatchNotes.ForeColor = System.Drawing.Color.White;
+            this.txtPatchNotes.Location = new System.Drawing.Point(329, 130);
+            this.txtPatchNotes.Multiline = true;
+            this.txtPatchNotes.Name = "txtPatchNotes";
+            this.txtPatchNotes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPatchNotes.Size = new System.Drawing.Size(225, 161);
+            this.txtPatchNotes.TabIndex = 4;
+            this.txtPatchNotes.Text = "Click play to launch Everquest";
+            // 
+            // btnMinimise
+            // 
+            this.btnMinimise.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinimise.FlatAppearance.BorderSize = 0;
+            this.btnMinimise.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnMinimise.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnMinimise.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimise.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimise.ForeColor = System.Drawing.Color.Transparent;
+            this.btnMinimise.Location = new System.Drawing.Point(534, 2);
+            this.btnMinimise.Name = "btnMinimise";
+            this.btnMinimise.Size = new System.Drawing.Size(20, 18);
+            this.btnMinimise.TabIndex = 5;
+            this.btnMinimise.UseVisualStyleBackColor = false;
+            this.btnMinimise.Click += new System.EventHandler(this.btnMinimise_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Transparent;
+            this.btnClose.Location = new System.Drawing.Point(568, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 18);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::P99Patcher.Properties.Resources.project1999;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(596, 395);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMinimise);
+            this.Controls.Add(this.txtPatchNotes);
+            this.Controls.Add(this.btnOptions);
+            this.Controls.Add(this.btnAccount);
+            this.Controls.Add(this.btnWeb);
+            this.Controls.Add(this.btnPlay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Project99 Patcher";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnWeb;
+        private System.Windows.Forms.Button btnAccount;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.TextBox txtPatchNotes;
+        private System.Windows.Forms.Button btnMinimise;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ P99Patcher/frmMain.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Diagnostics;
+
+namespace P99Patcher
+{    
+    public partial class frmMain : Form
+    {
+        public string EQPath = "";
+
+        public frmMain()
+        {
+            InitializeComponent();
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseMove);
+
+        }
+
+        private Point mouseOffset;
+        private bool isMouseDown = false;
+
+        private void frmMain_MouseDown(object sender,
+System.Windows.Forms.MouseEventArgs e)
+        {
+            int xOffset;
+            int yOffset;
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset = -e.X - SystemInformation.FrameBorderSize.Width;
+                yOffset = -e.Y -
+            SystemInformation.CaptionHeight - SystemInformation.FrameBorderSize.Height;
+
+                mouseOffset = new Point(xOffset, yOffset);
+                isMouseDown = true;
+            }
+        }
+        private void frmMain_MouseMove(object sender,
+        System.Windows.Forms.MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;
+            }
+        }
+
+        private void frmMain_MouseUp(object sender,
+        System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+            }
+        }
+
+        private void LoadSettings()
+        {
+            
+
+            string path = Application.StartupPath + "\\settings.xml";
+            try
+            {
+                XmlReader reader = XmlReader.Create(path);
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.Name.ToString() == "path")
+                        {
+                            this.EQPath = reader.ReadString().ToString();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                ConfigurePath();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimise_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnAccount_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.eqemulator.org/forums/profile.php?do=loginserver";
+            System.Diagnostics.Process.Start(url);
+        }
+
+        private void btnWeb_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.project1999.org";
+            System.Diagnostics.Process.Start(url);
+        }
+
+        private void ConfigurePath()
+        {
+            MessageBox.Show("Please locate your EQGame.exe in your Everquest folder");
+
+            OpenFileDialog ofn = new OpenFileDialog();
+            ofn.Filter = "EQGame.exe (*.exe)|*.exe";
+            ofn.Title = "Find EQGame.exe";
+            string FilePath = "";
+            if (ofn.ShowDialog() == DialogResult.Cancel)
+            {
+                NoFile();
+            }
+            else
+            {
+                try
+                {
+                    FilePath = ofn.FileName;
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error opening file", "File Error",
+                                     MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+
+            if (FilePath != "")
+            {
+                if (DeleteXML())
+                {
+                    string oldpath = FilePath;
+                    string newpath = oldpath.Replace("eqgame.exe", "eqhost.txt");
+                    File.Delete(newpath);
+                    File.Copy(Application.StartupPath + "\\eqhost.txt", newpath, true);
+                    this.EQPath = oldpath;
+                    SaveXML();
+                }
+                // close and pass
+            }
+            else
+            {
+                MessageBox.Show("Error: Blank filepath", "File Error",
+                                     MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private bool DeleteXML()
+        {
+            string path = Application.StartupPath + "\\settings.xml";
+            try
+            {
+                File.Delete(path);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void SaveXML()
+        {
+
+            string path = Application.StartupPath + "\\settings.xml";
+            try
+            {
+                XmlTextWriter textWriter = new XmlTextWriter(path, null);
+                textWriter.WriteStartDocument();
+                textWriter.WriteStartElement("connect");
+
+                textWriter.WriteStartElement("path");
+                textWriter.WriteString(this.EQPath);
+                textWriter.WriteEndElement();
+
+                textWriter.WriteEndElement();
+
+                textWriter.WriteEndDocument();
+                textWriter.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error writing file, is it read only?");
+            }
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            LoadSettings();
+        }
+
+        private void NoFile()
+        {
+            MessageBox.Show("Could not locate EQGame.exe, exiting.", "Missing EQ Folder", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            Application.Exit();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "eqgame.exe";
+            string oldpath = this.EQPath;
+            string justdir = oldpath.Replace("\\eqgame.exe", "");
+            startInfo.WorkingDirectory = @justdir;
+            startInfo.Arguments = "patchme";
+            Process process;
+            process = Process.Start(startInfo);
+        }
+
+        private void btnOptions_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "OptionsEditor.exe";
+            string oldpath = this.EQPath;
+            string justdir = oldpath.Replace("\\eqgame.exe", "");
+            startInfo.WorkingDirectory = @justdir;
+            Process process;
+            process = Process.Start(startInfo);
+        }
+    }
+}
